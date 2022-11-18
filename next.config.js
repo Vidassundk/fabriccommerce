2515e9bc2982a9c1f9abe0423b8d118c8cfb2ca6
Vidassundk <vidasstirbys@Vidass-MBP.lan>
2022-11-18 21:44:56 +0200

    initial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "rb.gy",
+      "images.unsplash.com",
+      "cdn.sanity.io",
+      "lh3.googleusercontent.com",
+    ],
+  },
+};
